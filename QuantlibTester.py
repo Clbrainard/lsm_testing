972e7c6e4c7a6311ce15821106ffea102a08a735
niarble <lloydbrainard@gmail.com>
2026-04-24 22:10:46 -0700

    more data

--- QuantlibTester.py
+++ QuantlibTester.py
@@ -21,7 +21,7 @@ import QuantLib as ql
 CSV_PATH    = "NTestSet.csv"
 OUTPUT_PATH = "C1-results.csv"
 P           = 10000
-N_VALUES    = [5,50,500,5000,50000,100000]
+N_VALUES    = [50,75,125,150]
 N_TESTS     = 100
 SEEDS       = list(range(100))
 Q_DEFAULT   = 0.0
